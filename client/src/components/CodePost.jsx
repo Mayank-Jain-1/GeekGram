9b9454f4ab1e1be72e24d645f08c1972b7719f62
Mayank <kaushalmayank.k@gmail.com>
2023-02-06 19:25:56 +0530

    frontend folder name changed

---
+++ client/src/components/CodePost.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { BsDot, BsThreeDots } from "react-icons/bs";
+import { FaCheck } from "react-icons/fa";
+import { GoIssueOpened } from "react-icons/go";
+import { IoCheckmarkDoneSharp } from "react-icons/io5";
+import remarkGfm from 'remark-gfm'
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const CodePost = ({ languages = "HTML", status, problemStatement }) => {
+  problemStatement =
+    "Issue with some react component. Cannot open it using this onClick Function";
+  status = "";
+
+  const [input, setInput] = useState(`
+  ## This is 
+  `);
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const highlighter = (value, language) => {
+    const codeString = "(num) => num + 1";
+    return (
+      <SyntaxHighlighter language={language} style={tomorrow}>
+        {value}{" "}
+      </SyntaxHighlighter>
+    );
+  };
+
+  return (
+    <div className="border-y border-brandGrey mb-10">
+      <section className="header w-full h-16 flex justify-between items-center px-5 text-xs">
+        <div className="flex items-center">
+          <div className="flex items-center border-r border-brandGrey flex-shrink-0">
+            <a href="" className="">
+              <img
+                src="https://robohash.org/hicveldicta.png"
+                alt=""
+                className="w-10 h-10 rounded-full bg-black"
+              />
+            </a>
+            <a href="">
+              <p className="mx-3">Name</p>
+            </a>
+          </div>
+          <div className="flex items-center space-x-3 px-3">
+            <p>{languages}</p>
+            <span className="flex items-center">
+              <BsDot className="text-brandDark" />
+              <p className="text-brandDark">21h</p>
+            </span>
+            {status === "verified" ? (
+              <IoCheckmarkDoneSharp className="text-xl text-brandAqua" />
+            ) : status === "answered" ? (
+              <FaCheck className="text-xl text-brandAqua " />
+            ) : (
+              <GoIssueOpened className="text-xl text-yellow-500" />
+            )}
+          </div>
+        </div>
+
+        <button className="relative -right-4 p-3 group">
+          <BsThreeDots className="text-xl group-hover:text-brandDark" />
+        </button>
+      </section>
+
+      <div className="h-auto overflow-hidden duration-300">
+        <section className="problem p-2 border border-brandGrey">
+          <div className="w-full h-12 bg-red-400 flex items-center">
+              <button>Raw</button>
+              <button>Preview</button>
+              <button></button>
+          </div>
+
+          <textarea
+            onChange={handleChange}
+            className="bg-brandGrey h-80 w-full"
+            type="text"
+            value={input}
+          />
+          <ReactMarkdown children={input}
+            remarkPlugins={[remarkGfm]}
+            className="w-full "
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    children={String(children).replace(/\n$/, "")}
+                    style={tomorrow}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  />
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+          />
+        </section>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default CodePost;
